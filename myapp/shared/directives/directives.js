824e8813e70ea74fbab4c27bfbb0fa5d751412f9
rafneesh <rafneesh@gmail.com>
2016-02-03 12:11:31 +0530

    replaced wit sample project

---
+++ myapp/shared/directives/directives.js
@@ -0,0 +1,21 @@
+myApp.directive('footer',function(){
+
+	return{
+		restrict: 'A',
+		replace: true,
+		templateUrl: "./myapp/shared/footer.html",
+		controller:['$scope', '$filter', function ($scope, $filter) {
+        }]
+	}
+});
+
+myApp.directive('header',function(){
+
+	return{
+		restrict: 'A',
+		replace: true,
+		templateUrl: "./myapp/shared/header.html",
+		controller:['$scope', '$filter', function ($scope, $filter) {
+        }]
+	}
+})
